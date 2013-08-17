45a48e4c62fceaba26f8e2448a4f0cb219958336
Abigail Watson <awatson1978@gmail.com>
2013-08-17 00:28:57 -0400

    added easings files, transition css files, and jquery.easing.js; also added dependency to less

--- package.js
+++ package.js
@@ -3,9 +3,26 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['iron-router', 'templating'], 'client');
+  api.use(['iron-router', 'templating', 'less'], 'client');
   api.add_files([
-    'transitionerPanes.html', 
+      'jquery.easing.1.3.js',
+
+      'easings.less',
+      'jquery.mobile.transition.css',
+      'jquery.mobile.transition.fade.css',
+      'jquery.mobile.transition.flip.css',
+      'jquery.mobile.transition.flow.css',
+      'jquery.mobile.transition.pop.css',
+      'jquery.mobile.transition.slide.css',
+      'jquery.mobile.transition.slidedown.css',
+      'jquery.mobile.transition.slidefade.css',
+      'jquery.mobile.transition.slidein.keyframes.css',
+      'jquery.mobile.transition.slideout.keyframes.css',
+      'jquery.mobile.transition.slideup.css',
+      'jquery.mobile.transition.turn.css',
+      'jquery.mobile.transition.visuals.css',
+
+      'transitionerPanes.html',
     'transitionerPanes.js'
   ], 'client')
 });
@@ -19,6 +36,23 @@ Package.on_test(function(api) {
   ]);
   
   api.add_files([
+      'jquery.easing.1.3.js',
+
+      'easings.less',
+      'jquery.mobile.transition.css',
+      'jquery.mobile.transition.fade.css',
+      'jquery.mobile.transition.flip.css',
+      'jquery.mobile.transition.flow.css',
+      'jquery.mobile.transition.pop.css',
+      'jquery.mobile.transition.slide.css',
+      'jquery.mobile.transition.slidedown.css',
+      'jquery.mobile.transition.slidefade.css',
+      'jquery.mobile.transition.slidein.keyframes.css',
+      'jquery.mobile.transition.slideout.keyframes.css',
+      'jquery.mobile.transition.slideup.css',
+      'jquery.mobile.transition.turn.css',
+      'jquery.mobile.transition.visuals.css',
+
     'transitionerPanes_test.html',
     'transitionerPanes_test.js'
   ], 'client')
